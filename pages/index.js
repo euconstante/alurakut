7c0c9e9b65942b32e792b32297e73f4ec46ca7b5
Bianca Constante <bianca.guedert@jungledevs.com>
2021-07-13 18:28:38 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import MainGrid from '../src/components/mainGrid'
-import Box from '../src/components/Box'
+import Box from '../src/components/box'
 import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
